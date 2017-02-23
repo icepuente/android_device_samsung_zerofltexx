a28fb337c6c7567123b2e6710de04209de75c670
Alec Puente <icepuente@gmail.com>
2017-02-22 22:11:46 -0700

    first commit

---
+++ device.mk
@@ -0,0 +1,41 @@
+#
+# Copyright (C) 2015 The Android Open Kang Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+LOCAL_PATH := device/samsung/zerofltexx
+
+## device overlays
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+
+# Carrier init
+PRODUCT_PACKAGES += \
+    init.carrier.rc
+
+# Set usb type
+ADDITIONAL_DEFAULT_PROPERTIES += \
+    persist.service.adb.enable=1 \
+    ro.adb.secure=0 \
+    ro.secure=0 \
+    ro.allow.mock.location=1
+
+# cpboot daemon
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/ril/sbin/cbd:root/sbin/cbd
+
+# Inherit from zero-common
+$(call inherit-product, device/samsung/zero-common/zero-common.mk)
+
+# Proprietary
+$(call inherit-product, device/samsung/zerofltexx/proprietary.mk)
